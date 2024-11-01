b31bcecc41de1172e541890660fb80652bbffe12
Raph <raph4sure007@gmail.com>
2024-11-01 13:35:50 +0100

    Done for now

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name?\n")
+age = input("How old are you?\n")
+location = input("What is your location?\n")
+print(f"Hello {name}, you are {age} years old and live in {location}")
+
